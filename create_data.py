aff37c14d7ac8ede119b686a06610b34f576eb6e
CSUBioGroup <limin@csu.edu.cn>
2025-06-17 21:53:25 +0800

    Update create_data.py

--- create_data.py
+++ create_data.py
@@ -120,7 +120,7 @@ for dataset in datasets:
         train_XD = [torch.LongTensor(tokenizer.parse(smile)) for smile in train_MTS]
 
         # Process test set
-        test_drugs, test_MTS, test_prots, test_Y = list(df_test['compound_iso_smiles']), list(df_train['target_smiles']), list(df_test['target_sequence']), list(df_test['affinity'])
+        test_drugs, test_MTS, test_prots, test_Y = list(df_test['compound_iso_smiles']), list(df_test['target_smiles']), list(df_test['target_sequence']), list(df_test['affinity'])
         XT = [seq_cat(t) for t in test_prots]
         test_drugs, test_MTS, test_prots, test_Y = np.asarray(test_drugs), np.asarray(test_MTS), np.asarray(XT), np.asarray(test_Y)
         test_XD = [torch.LongTensor(tokenizer.parse(smile)) for smile in test_MTS]
